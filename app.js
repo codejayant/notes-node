02dce49f49ad7e77e1f1898a94fc9f2efec23dda
jayant sarkar <codejayant@gmail.com>
2017-08-15 22:08:04 -0700

    print all command line argument

--- app.js
+++ app.js
@@ -5,4 +5,18 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
+var command = process.argv[2];
+console.log('Command : ', command);
 console.log(process.argv);
+
+if (command === 'add')	{
+	console.log('Adding new note');
+} else if (command === 'list')	{
+	console.log('Listing all notes');
+} else if (command === 'read')	{
+	console.log('Read all notes');
+} else if (command === 'remove') {
+	console.log('Remove a note');
+} else {
+	console.log('command not found');
+}
