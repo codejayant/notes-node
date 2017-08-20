74d68da80518da818c17256eb34a26228f092420
jayant sarkar <codejayant@gmail.com>
2017-08-19 21:12:41 -0700

    read and write json file using fs

--- playground/json.js
+++ playground/json.js
@@ -1,12 +1,28 @@
-var obj = {
-	name: 'Jayant'
+// var obj = {
+// 	name: 'Jayant'
+// };
+
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name": "jayant", "age": 25}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+	title: 'some title',
+	body: 'some body'
 };
 
-var stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+var originalNoteString = JSON.stringify(originalNote);
 
-var personString = '{"name": "jayant", "age": 25}';
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
