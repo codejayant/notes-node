e0e9539433fa342c67f244258ed0125303f84027
jayant sarkar <codejayant@gmail.com>
2017-08-19 20:42:18 -0700

    yargs command with new feature like add, list, read and remove

--- app.js
+++ app.js
@@ -2,21 +2,29 @@ console.log('Starting app');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2];
+var argv = yargs.argv;
+
+var command = argv._[0];
 console.log('Command : ', command);
-console.log(process.argv);
+console.log('Process : ', process.argv);
+console.log('yargs : ', argv);
 
 if (command === 'add')	{
 	console.log('Adding new note');
+	notes.addNote(argv.title, argv.body);
 } else if (command === 'list')	{
 	console.log('Listing all notes');
+	notes.getAll();
 } else if (command === 'read')	{
 	console.log('Read all notes');
+	notes.readNote(argv.title);
 } else if (command === 'remove') {
 	console.log('Remove a note');
+	notes.removeNote(argv.title);
 } else {
 	console.log('command not found');
 }
--- notes.js
+++ notes.js
@@ -1,10 +1,25 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-	console.log('addNote')
-	return 'New Note';
+var addNote = (title, body) => {
+	console.log('Adding note', title, body);
 };
 
-module.exports.add = (a, b) => {
-	return a + b;
+
+var getAll = () => {
+	console.log('Getting all notes');
+};
+
+var readNote = (title) => {
+	console.log("Reading note : ", title);
+};
+
+var removeNote = (title) => {
+	console.log(`Removing note : ${title}`);
+};
+
+module.exports = {
+	addNote,
+	getAll,
+	readNote,
+	removeNote
 };
